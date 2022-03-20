e55b7e7a560a54d010836e4e893a8405ff30a371
Natchanon Wangpruek <lnwnut227@gmail.com>
2022-03-21 02:45:22 +0700

    Add picking up system

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -21,6 +21,16 @@ public class BuildManager : MonoBehaviour
     public Tile target;
     Vector3 targetPos;
 
+    public bool isSelected = false;
+
+    public int handItem = 0;
+
+    public bool inHand = false;
+
+    public Transform handGridUI;
+
+    public List<GameObject> UIHands;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -37,64 +47,104 @@ public class BuildManager : MonoBehaviour
             Color tileColor = UIImage.color;
             tileColor.a = 0.5f;
 
-            if (i == selectedTower) tileColor.a = 1f;
+            if (i == selectedTower && isSelected) tileColor.a = 1f;
 
             UIImage.color = tileColor;
             UITowers.Add(UITower);
 
             i++;
         }
+        foreach (Tile towerIcon in towerIcons)
+        {   
+            GameObject UIHand = new GameObject("UI Tower");
+            UIHand.transform.parent = handGridUI;
+            UIHand.transform.localScale = new Vector3(1f, 1f, 1f);
 
-        targetPos = GetTowerPos();
+            Image UIImage = UIHand.AddComponent<Image>();
+            UIImage.sprite = towerIcon.sprite;
+
+            Color tileColor = UIImage.color;
+            tileColor.a = 0f;
+
+            UIImage.color = tileColor;
+            UIHands.Add(UIHand);
+            if(i==5) break;
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    async void Update()
     {
         targetTilemap.SetTile(targetTilemap.WorldToCell(targetPos), null);
         Vector3 towerPos = GetTowerPos();
-        targetTilemap.SetTile(targetTilemap.WorldToCell(towerPos), target);
+        if(isSelected || inHand){
+            targetTilemap.SetTile(targetTilemap.WorldToCell(towerPos), target);
+        }
         targetPos = towerPos;
 
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            selectedTower = 0;
-            RenderUITowers();
+            RenderUITowers(0);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            selectedTower = 1;
-            RenderUITowers();
+            RenderUITowers(1);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            selectedTower = 2;
-            RenderUITowers();
+            RenderUITowers(2);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha4))
         {
-            selectedTower = 3;
-            RenderUITowers();
+            RenderUITowers(3);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha5))
         {
-            selectedTower = 4;
-            RenderUITowers();
+            RenderUITowers(4);
         }
         else if (Input.GetKeyDown(KeyCode.Space) && isTileAvailable(towerPos))
         {
-            tilemap.SetTile(tilemap.WorldToCell(towerPos), towers[selectedTower]);
+            if (isSelected)
+                tilemap.SetTile(tilemap.WorldToCell(towerPos), towers[selectedTower]);
         }
         else if (Input.GetKeyDown(KeyCode.X))
         {
             tilemap.SetTile(tilemap.WorldToCell(towerPos), null);
         }
+        else if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if(!isTileAvailable(towerPos) && isSelected) {
+                for (int i = 0; i < 5; i++) {
+                    if (tilemap.GetTile(Vector3Int.FloorToInt(towerPos)) == towers[i])
+                    {
+                        handItem = i;
+                    }
+                }
+                inHand = true;
+                isSelected = false;
+                RenderUITowers(0);
+                RenderUIHand();
+                tilemap.SetTile(tilemap.WorldToCell(towerPos), null);
+            }
+            else if(inHand) {
+                inHand = false;
+                RenderUIHand();
+                tilemap.SetTile(tilemap.WorldToCell(towerPos), towers[handItem]);
+            }
+        }
 
         Debug.Log(tilemap.GetTile(Vector3Int.FloorToInt(towerPos)));
     }
 
-    void RenderUITowers()
+    void RenderUITowers(int selecter)
     {
+        if((selectedTower == selecter && isSelected) || inHand) {
+            this.isSelected = false;
+        }
+        else {
+            this.selectedTower = selecter;
+            this.isSelected = true;
+        }
         int i = 0;
         foreach (GameObject tower in UITowers)
         {
@@ -102,11 +152,36 @@ public class BuildManager : MonoBehaviour
             Color tileColor = UIImage.color;
             tileColor.a = 0.5f;
 
-            if (i == selectedTower) tileColor.a = 1f;
+            if (i == selectedTower && isSelected) tileColor.a = 1f;
 
             UIImage.color = tileColor;
 
             i++;
+        }
+    }
+
+    void RenderUIHand()
+    {
+        if (inHand) {
+            int i = 0;
+            foreach (GameObject UIhand in UIHands)
+            {
+                Image UIImage = UIhand.GetComponent<Image>();
+                Color tileColor = UIImage.color;
+                tileColor.a = 1f;
+                UIImage.sprite = towerIcons[handItem].sprite;
+                UIImage.color = tileColor;
+                i++;
+            }
+        }
+        else {
+            foreach (GameObject UIhand in UIHands)
+            {
+                Image UIImage = UIhand.GetComponent<Image>();
+                Color tileColor = UIImage.color;
+                tileColor.a = 0f;
+                UIImage.color = tileColor;
+            }
         }
     }
 
