4f1bdc947a10e3f3aefa8dd8be55e365b0d3bb5c
brewerywiwis <brewder136@hotmail.com>
2022-03-21 18:44:12 +0700

    add ghost enemy with die animation, blue fire effect

---
+++ Assets/Scripts/Enemy/Ghost/GhostDie.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostDie : MonoBehaviour
+{
+    Animator animator;
+    int count = 5000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("DEAD")){
+            Destroy(gameObject);
+        }
+        else if (count == 0 ){
+            animator.SetBool("isDie", true);
+        }
+
+        count--;
+        Debug.Log("Count: " + count);
+    }
+}
