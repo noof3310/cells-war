48ea57d665bbfdbf8df264c64e1f9037d0a4abf3
palmmy2542 <palm_my2542@hotmail.com>
2022-04-26 02:05:38 +0700

    fix

--- Assets/Scripts/Player/PlayerDetails.cs
+++ Assets/Scripts/Player/PlayerDetails.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerDetails : MonoBehaviour
 {
-    public static float whiteBloodCellNumber = 1000;
+    public static float whiteBloodCellNumber;
     // Start is called before the first frame update
     void Start()
     {
