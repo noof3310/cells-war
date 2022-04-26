e893deec0953f64c009d0ebb511f882a40dde793
brewerywiwis <brewder136@hotmail.com>
2022-04-26 21:25:29 +0700

    hide control panel in status bar

--- Assets/Scripts/Player/PlayerDetails.cs
+++ Assets/Scripts/Player/PlayerDetails.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerDetails : MonoBehaviour
 {
-    public static float whiteBloodCellNumber;
+    public static float whiteBloodCellNumber = 100;
     // Start is called before the first frame update
     void Start()
     {
