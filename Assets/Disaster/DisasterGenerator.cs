6e8c721a38d6b3ae5ff75e0f2f791e02c43afa21
palmmy2542 <palm_my2542@hotmail.com>
2022-04-26 21:50:58 +0700

    Fix

--- Assets/Disaster/DisasterGenerator.cs
+++ Assets/Disaster/DisasterGenerator.cs
@@ -11,7 +11,6 @@ public class DisasterGenerator : MonoBehaviour
     public int fireAmount = 50;
     public int Radius = 50;
     public List<GameObject> fireList = new List<GameObject>();
-    public GameObject typhoonPreFab;
     public Vector3 position = new Vector3(-1.5f, 39f, -2f);
     public float chanceForDisaster = 0.3f;
     public static Disaster selectedDisaster;
@@ -31,7 +30,7 @@ public class DisasterGenerator : MonoBehaviour
         if (GameManager.State == GameState.RushState && !generateSuccess)
         {
             generateSuccess = true;
-            Disaster disaster = (Disaster)Random.Range(0, System.Enum.GetValues(typeof(Disaster)).Length);
+            Disaster disaster = (Disaster)Random.Range(1, System.Enum.GetValues(typeof(Disaster)).Length);
             float rand = Random.Range(0f, 1f);
             if (chanceForDisaster > rand)
             {
@@ -95,10 +94,6 @@ public class DisasterGenerator : MonoBehaviour
                     to.GetComponent<TowerBehavior>().Died();
                 }
                 break;
-            // case Disaster.Typhoon:
-            //     selectedDisasterObject = Instantiate(typhoonPreFab, position, Quaternion.identity);
-            //     selectedDisasterObject.name = "Disaster";
-            //     break;
             default:
                 if (displayText != null)
                     Destroy(displayText);
@@ -113,5 +108,4 @@ public enum Disaster
     Unknown,
     Rain,
     Fire,
-    Typhoon
 }
